9d86beae292cc1bcccafb226963030ddacf0bf7b
ledonhome.webdev3 <56617379+LOHwebdev3@users.noreply.github.com>
2024-03-29 10:11:53 +0700

    Update App.js

--- 3.Chmod/src/App.js
+++ 3.Chmod/src/App.js
@@ -1,35 +1,34 @@
-import "./styles.css";
 export default function App() {
-  return (
-    <div className="App">
-      <Chmod permission={"rwxrwxr-x"} /> {/*775*/}
-      <Chmod permission={"-r-w------"} /> {/*600*/}
-      <Chmod permission={"r-wr----r"} /> {/*644*/}
-      <Chmod permission={"-rwxrwxr-x"} /> {/*775*/}
-      <Chmod permission={"-r-x---r-x"} /> {/*505*/}
-      <Chmod permission={"-rw-rw-rw-"} /> {/*666*/}
-      <Chmod permission={"-r--r--r--"} /> {/*444*/}
-      <Chmod permission={"---x--x--x"} /> {/*111*/}
-      <Chmod permission={"-rw-------"} /> {/*600*/}
-      <Chmod permission={"-rw-rw-r--"} /> {/*644*/}
-    </div>
-  );
+    return (
+        <div className="App">
+            <Chmod permission={"rwxrwxr-x"} answer={'775'} /> {/*775*/}
+            <Chmod permission={"-r-w------"} answer={'600'} /> {/*600*/}
+            <Chmod permission={"r-wr----r"} answer={'644'} /> {/*644*/}
+            <Chmod permission={"-rwxrwxr-x"} answer={'775'} /> {/*775*/}
+            <Chmod permission={"-r-x---r-x"} answer={'505'} /> {/*505*/}
+            <Chmod permission={"-rw-rw-rw-"} answer={'666'} /> {/*666*/}
+            <Chmod permission={"-r--r--r--"} answer={'444'} /> {/*444*/}
+            <Chmod permission={"---x--x--x"} answer={'111'} /> {/*111*/}
+            <Chmod permission={"-rw-------"} answer={'600'} /> {/*600*/}
+            <Chmod permission={"-rw-rw-r--"} answer={'644'} /> {/*644*/}
+        </div>
+    );
 }
 
 const Chmod = (props) => {
-  const { permission } = props;
-  function convert(input) {
-    // code here.
-    const result = "";
-    return result;
-  }
+    const { permission,answer } = props;
+    function convert(input) {
+        // code here.
+        const result = "transform me";
+        return `${result} is ${answer === result ? 'true' : 'false'}`;
+    }
 
-  return (
-    <>
-      <p>
-        {`[ ${permission} ] =  `}
-        {convert(permission)}
-      </p>
-    </>
-  );
+    return (
+        <>
+            <p>
+                {`[ ${permission} ] =  `}
+                {convert(permission)}
+            </p>
+        </>
+    );
 };
